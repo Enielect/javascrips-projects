6b1f067c672241b7279bce90e1d2038f137ca9a9
Enielect <eniolafaithful57@gmail.com>
2023-08-05 11:31:00 +0100

    update file name

--- nav_bar/script.js
+++
@@ -1,5 +0,0 @@
-let nav_toggle = document.querySelector('.nav-toggle');
-let links = document.querySelector('.links')
-nav_toggle.addEventListener('click', () => {
-    links.classList.toggle('show-links');
-})
