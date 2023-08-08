224133475a461ea89855d163d163d8d6406457ea
Enielect <eniolafaithful57@gmail.com>
2023-08-08 04:21:24 +0100

    create menu array to store info

---
+++ menu-list/script.js
@@ -0,0 +1,114 @@
+const menu = [
+    {
+        id: 1,
+        title: 'butttermilk pancakes',
+        category: 'breakfast',
+        price: 15.99,
+        img: 'images/img-1.jpeg',
+        desc: `Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Eligendi officiis ab, nihil
+        reprehenderit tempore cum quae accusantium
+        dolores?`
+    },
+    {
+        id: 2,
+        title: 'dinner double',
+        category: 'lunch',
+        price: 12.99,
+        img: 'images/img-2.jpeg',
+        desc: `Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Eligendi officiis ab, nihil
+        reprehenderit tempore cum quae accusantium
+        dolores?`
+    },
+    {
+        id: 3,
+        title: 'country delight',
+        category: 'lunch',
+        price: 8.99,
+        img: 'images/img-2.jpeg',
+        desc: `Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Eligendi officiis ab, nihil
+        reprehenderit tempore cum quae accusantium
+        dolores?`
+    },
+    {
+        id: 1,
+        title: 'orco dream',
+        category: 'breakfast',
+        price: 15.99,
+        img: 'images/img-4.jpeg',
+        desc: `Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Eligendi officiis ab, nihil
+        reprehenderit tempore cum quae accusantium
+        dolores?`
+    },
+    {
+        id: 1,
+        title: 'fluffy doughnut',
+        category: 'breakfast',
+        price: 7.99,
+        img: 'images/img-9.jpeg',
+        desc: `Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Eligendi officiis ab, nihil
+        reprehenderit tempore cum quae accusantium
+        dolores?`
+    },
+    {
+        id: 1,
+        title: 'creamy milkshake',
+        category: 'shake',
+        price: 15.99,
+        img: 'images/img-7.jpeg',
+        desc: `Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Eligendi officiis ab, nihil
+        reprehenderit tempore cum quae accusantium
+        dolores?`
+    },
+    {
+        id: 1,
+        title: 'meat attack',
+        category: 'lunch',
+        price: 18.99,
+        img: 'images/img-6.jpeg',
+        desc: `Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Eligendi officiis ab, nihil
+        reprehenderit tempore cum quae accusantium
+        dolores?`
+    },
+    {
+        id: 1,
+        title: 'american classic',
+        category: 'breakfast',
+        price: 30.99,
+        img: 'images/img-5.jpeg',
+        desc: `Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Eligendi officiis ab, nihil
+        reprehenderit tempore cum quae accusantium
+        dolores?`
+    },
+    {
+        id: 1,
+        title: 'cripsy fries',
+        category: 'lunch',
+        price: 11.99,
+        img: 'images/img-3.jpeg',
+        desc: `Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Eligendi officiis ab, nihil
+        reprehenderit tempore cum quae accusantium
+        dolores?`
+    },
+    {
+        id: 1,
+        title: 'hamburger',
+        category: 'breakfast',
+        price: 5.99,
+        img: 'images/img-1.jpeg',
+        desc: `Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Eligendi officiis ab, nihil
+        reprehenderit tempore cum quae accusantium
+        dolores?`
+    },
+]
+
+let all = document.querySelectorAll('button');
