4f6a340a476a4c4a081238e6971c52d3194dacad
Enielect <eniolafaithful57@gmail.com>
2023-08-14 11:25:21 +0100

    add top-link button javascript fucntionality

--- scroll-project/script.js
+++ scroll-project/script.js
@@ -1,9 +1,8 @@
 let button = document.querySelector('.button-toggle');
-let links = document.querySelector('.links');
+let linksContainer = document.querySelector('.links');
+let links = document.querySelector('.link-container') 
 
-button.addEventListener('click', () => {
-    links.classList.toggle('show-links');  
-})
+
 
 // setting the date functionality
 
@@ -11,3 +10,40 @@ let date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 
 // close links
+button.addEventListener('click', () => {
+    // calculating the height 
+    // Element.getBoundingClientRect () method returns the size of an Element
+    // and its position relative to the viewport
+    // links.classList.toggle('show-links');
+    const containerHeight = linksContainer.getBoundingClientRect().height;  
+    const linksHeight = links.getBoundingClientRect().height;
+    console.log(containerHeight)
+    console.log(linksHeight)
+    if(containerHeight == 0) {
+        linksContainer.style.height = `${linksHeight}px`
+    }else {linksContainer.style.height = 0;}
+});
+
+// fixed nav-bar
+// pageYOffset is a read - only window property that returns the 
+// number of pixels the document has been scrolled vertically 
+const navbar = document.getElementById('nav');
+const toplink = document.querySelector('.button-top');
+
+window.addEventListener('scroll', () => {
+    let navHeight = navbar.getBoundingClientRect().height;
+    console.log(navHeight)
+    let scrollHeight = window.pageYOffset;
+    console.log(scrollHeight)
+    if(scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav');
+    } else {
+        navbar.classList.remove('fixed-nav');
+    }
+
+    if(scrollHeight > 500) {
+        toplink.classList.add('show-link');
+    }else {
+        toplink.classList.remove('show-link')
+    }
+})
