c4cec7d00c9812f13c1d3bce15edfba9ff6c603e
Enielect <eniolafaithful57@gmail.com>
2023-08-14 10:15:08 +0100

    removed position absolute from nav to enable the height developed from clicking the nav bar is maintained in the flow of the document

--- scroll-project/script.js
+++ scroll-project/script.js
@@ -1,12 +1,13 @@
-let button = document.querySelector('button');
+let button = document.querySelector('.button-toggle');
 let links = document.querySelector('.links');
-let image = document.querySelector('.image')
+
 button.addEventListener('click', () => {
-    links.classList.toggle('show-links');
-    image.classList.toggle('show-image');
+    links.classList.toggle('show-links');  
 })
 
 // setting the date functionality
 
 let date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
+
+// close links
